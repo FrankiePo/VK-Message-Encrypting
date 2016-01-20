f6ada108fa42af765c931bdb5800c6cc0cf4d3fb
Sergey Koshechkin <tritel59@gmail.com>
2016-01-20 09:42:17 +0300

    Codestyle mistake

--- vk.js
+++ vk.js
@@ -204,7 +204,6 @@ function getKey() {
                                     }
                                 }
                             }
-
                         }
                     }
                 }
